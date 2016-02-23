086654a26bcf790cd0c8661c163fbac7aea6893d
Konstantin <hs@ngs.ru>
2016-02-23 22:43:48 +0600

    supplier places (google maps)

---
+++ src/main/java/app/model/api/PlaceListRes.java
@@ -0,0 +1,23 @@
+package app.model.api;
+
+import app.model.Place;
+import app.model.Supplier;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PlaceListRes {
+
+    @JsonProperty("data")
+    private List<Place> places;
+
+    public List<Place> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<Place> places) {
+        this.places = places;
+    }
+}
--- src/main/java/app/service/SupplierService.java
+++ src/main/java/app/service/SupplierService.java
@@ -1,11 +1,9 @@
 package app.service;
 
 import app.config.Config;
+import app.model.Place;
 import app.model.Supplier;
-import app.model.api.SupplierAddRes;
-import app.model.api.SupplierGetRes;
-import app.model.api.SupplierListRes;
-import app.model.api.SupplierUpdateRes;
+import app.model.api.*;
 import app.util.EntityBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -20,7 +18,8 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class SupplierService {
 
-    private final String path = "/supplier";
+    public static final String SUPPLIER_PATH = "/supplier";
+    public static final String PLACE_PATH = "/place";
 
     @Autowired
     private Config config;
@@ -31,42 +30,61 @@ public class SupplierService {
     @Autowired
     private EntityBuilder entityBuilder;
 
-    public SupplierGetRes read(Long id) {
-        HttpEntity requestEntity = entityBuilder.simpleEntity();
-        String uri = config.getUrl() + path + "/" + id;
-        ResponseEntity<SupplierGetRes> quote = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, SupplierGetRes.class);
-        return quote.getBody();
+    /*
+    Supplier
+     */
+
+    public List<Supplier> getSuppliers() {
+        return getListSuppliers().getSuppliers();
     }
 
-    public SupplierGetRes delete(Long id) {
-        String uri = config.getUrl() + path + "/" + id;
-        HttpEntity requestEntity = entityBuilder.simpleEntity();
-        ResponseEntity<SupplierGetRes> quote = restTemplate.exchange(uri, HttpMethod.DELETE, requestEntity, SupplierGetRes.class);
-        return quote.getBody();
-    }
-    
-    public SupplierAddRes create(Supplier supplier) {
-        String uri = config.getUrl() + path;
-        HttpEntity<Supplier> requestEntity = entityBuilder.bodyEntity(supplier);
-        ResponseEntity<SupplierAddRes> quote = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, SupplierAddRes.class);
-        return quote.getBody();
-    }
-
-    public SupplierUpdateRes update(Supplier supplier) {
-        String uri = config.getUrl() + path + "/" + supplier.getId();
-        HttpEntity<Supplier> requestEntity = entityBuilder.bodyEntity(supplier);
-        ResponseEntity<SupplierUpdateRes> quote = restTemplate.exchange(uri, HttpMethod.PUT, requestEntity, SupplierUpdateRes.class);
-        return quote.getBody();
-    }
-
-    public SupplierListRes getList() {
-        String uri = config.getUrl() + path;
+    public SupplierListRes getListSuppliers() {
+        String uri = config.getUrl() + SUPPLIER_PATH;
         HttpEntity requestEntity = entityBuilder.simpleEntity();
         ResponseEntity<SupplierListRes> quote = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, SupplierListRes.class);
         return quote.getBody();
     }
 
-    public List<Supplier> getSuppliers() {
-        return getList().getSuppliers();
+    public SupplierUpdateRes update(Supplier supplier) {
+        String uri = config.getUrl() + SUPPLIER_PATH + "/" + supplier.getId();
+        HttpEntity<Supplier> requestEntity = entityBuilder.bodyEntity(supplier);
+        ResponseEntity<SupplierUpdateRes> quote = restTemplate.exchange(uri, HttpMethod.PUT, requestEntity, SupplierUpdateRes.class);
+        return quote.getBody();
+    }
+
+    public SupplierAddRes create(Supplier supplier) {
+        String uri = config.getUrl() + SUPPLIER_PATH;
+        HttpEntity<Supplier> requestEntity = entityBuilder.bodyEntity(supplier);
+        ResponseEntity<SupplierAddRes> quote = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, SupplierAddRes.class);
+        return quote.getBody();
+    }
+
+    public SupplierGetRes read(Long id) {
+        HttpEntity requestEntity = entityBuilder.simpleEntity();
+        String uri = config.getUrl() + SUPPLIER_PATH + "/" + id;
+        ResponseEntity<SupplierGetRes> quote = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, SupplierGetRes.class);
+        return quote.getBody();
+    }
+
+    public SupplierGetRes delete(Long id) {
+        String uri = config.getUrl() + SUPPLIER_PATH + "/" + id;
+        HttpEntity requestEntity = entityBuilder.simpleEntity();
+        ResponseEntity<SupplierGetRes> quote = restTemplate.exchange(uri, HttpMethod.DELETE, requestEntity, SupplierGetRes.class);
+        return quote.getBody();
+    }
+
+    /*
+    Places
+     */
+
+    public List<Place> getPlaces(Long supplierId) {
+        return getListPlaces(supplierId).getPlaces();
+    }
+
+    public PlaceListRes getListPlaces(Long supplierId) {
+        String uri = config.getUrl() + SUPPLIER_PATH + "/" + supplierId + PLACE_PATH;
+        HttpEntity requestEntity = entityBuilder.simpleEntity();
+        ResponseEntity<PlaceListRes> quote = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, PlaceListRes.class);
+        return quote.getBody();
     }
 }
